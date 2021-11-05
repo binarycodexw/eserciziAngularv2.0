8c03942c2e9f36e85c5cbe175a75f9b005fde70d
binarycodexw <86565924+binarycodexw@users.noreply.github.com>
2021-11-05 15:41:46 +0100

    aggiungendo quarto esercizio

--- src/app/user-single/user-single.component.ts
+++ src/app/user-single/user-single.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { obj } from '../model2';
 
 @Component({
   selector: 'app-user-single',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-single.component.css']
 })
 export class UserSingleComponent implements OnInit {
+
+
+  @Input() user!: obj;
+  @Input() index!: number;
+
 
   constructor() { }
 
